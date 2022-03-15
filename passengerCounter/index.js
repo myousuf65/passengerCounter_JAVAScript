4f2dcd93fb0ea23ed91cc399df6032ab52ffc528
Muhammad Yousuf <official.yousufpathan@gmail.com>
2022-03-15 09:13:34 +0800

    Add files via upload

---
+++ passengerCounter/index.js
@@ -0,0 +1,23 @@
+
+let count_el = document.getElementById('count'); //pointing to the address of the element that has id of "count"
+let prev = document.getElementById('entries');
+let count = 0;
+
+
+
+
+function increment(){
+    count += 1;
+    count_el.innerText = count;
+    
+}
+
+function save(){
+    prev.textContent += count + " - ";
+    count = 0;
+    count_el.textContent = 0
+}
+
+
+
+
